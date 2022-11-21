3826b558c10a17b9fea5b8e7b798bbcfe0d0abf9
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:42:45 +0100

    Update fib.c

--- src/fib.c
+++ src/fib.c
@@ -8,25 +8,14 @@
 
 
 // fib 1 = 1
-// fib 2 = 1
-// fib 3 = 2
-// fib 4 = 3
-// fib 5 = 5
 
-/* Fibonacci function definition */
-int fib_tail (int n, int p,int pp)
-{
-/* assert to make sure there is at least 1 fib number */
-assert (n >= 1);
-
-    if(n == 1)
+int fib(int n, int p, int pp) {
+    // basecase is where n==1.
+    if (n == 1) {
         return p;
-
-    else
-        return fib_tail(n-1,pp+p,p);
-}
-
-
-int fib(int n) {
-    return fib_tail(n, 1, 0);
+    } else {
+       
+        return fib(n - 1, pp, p + pp);
+    }
+    return 0;
 }
