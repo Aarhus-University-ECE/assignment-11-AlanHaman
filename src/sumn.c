78e045a35cc8f421974e5df170a21b245ad61bca
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:57:46 +0100

    Update sumn.c

--- src/sumn.c
+++ src/sumn.c
@@ -1,14 +1,23 @@
 #include "sumn.h"
 
 #include <stdio.h>
+#include <assert.h>
 
-int sumn(int n) {
-    // basecase at n == 1.
-    if (n == 1) {
-        return n;
-    } else {
-       
-        return (2 * n - 1) + sumn(n - 1);
+int sumn(int n)
+{
+
+    /*Pre-Condition*/
+    assert(n >= 1);
+
+    /*Recusrion*/
+    if (n > 1)
+    {
+        return 2 * n - 1 + sumn(n - 1); // Recursion
     }
-    return 0;
+    /*Base Case*/
+
+    if (n == 1) // Recursion
+    {
+        return 1;
+    }
 }
