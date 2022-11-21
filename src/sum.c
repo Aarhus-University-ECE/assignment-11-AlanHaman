9f79e955fe93a6b1f0439519be668d2e7ddb2174
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:59:37 +0100

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -1,37 +1,55 @@
 #include "sum.h"
 
-#include <assert.h>
 #include <stdio.h>
+#include <assert.h>
 
 /* Sum integers 1 to n */
-int sum(int n) {
-    /* pre-condition */
-    assert(n >= 1);
-    /* post-condition */
-    if (n > 1)
-        return n + sum(n - 1);
-    else
-        return 1;
+int sum(int n)
+{
+  /* pre-condition */
+  assert(n >= 1);
+  /* post-condition */
+  if (n > 1)
+    return n + sum(n - 1);
+  else
+    return 1;
 }
 
 /* Sum integers 1 to n */
-int sumtail(int n, int total) {
-    // basecase is at n==1.
-    
-    if (n == 1) {
-        return n + total;
-    } else {
-        return sumtail(n - 1, n + total);
-    }
+int sumtail(int n, int total)
+{
+
+  /*Pre-condition*/
+  assert(n >= 1);
+
+  /*Base Case*/
+  if (n == 1) 
+  {
+    return 1 + total;
+  }
+  else if (n > 1) // Recursion
+  {
+    return sumtail(n - 1, n + total);
+  }
 }
 
 /* Sum integers 1 to n */
-int sumwhile(int n) {
-    int total = 0;
-    // While that runs as long as n is not 0, n!=0
-    while (n != 0) {
-        total = total + n;
-        n--;
-    }
-    return total;
+int sumwhile(int n)
+{
+  /*Pre-condition*/
+  assert(n >= 1);
+
+  
+  int sum = 0;
+
+  /*Loop that runs while n is not 0
+  while (n != 0)
+  {
+    sum += n; 
+    n--;      
+  }
+
+  printf("%d\n", sum); 
+
+  return sum; 
 }
