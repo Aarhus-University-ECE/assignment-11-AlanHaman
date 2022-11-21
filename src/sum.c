c0f307267f64f93bcea732dd5f54c9fd62784d5d
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:47:12 +0100

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -1,29 +1,37 @@
 #include "sum.h"
 
-#include<stdio.h>
-#include<assert.h>
+#include <assert.h>
+#include <stdio.h>
 
 /* Sum integers 1 to n */
-int sum (int n)
-{
-/* pre-condition */
-  assert (n >= 1);
-/* post-condition */
-  if (n > 1)
-    return n + sum (n - 1);
-  else
-    return 1;
+int sum(int n) {
+    /* pre-condition */
+    assert(n >= 1);
+    /* post-condition */
+    if (n > 1)
+        return n + sum(n - 1);
+    else
+        return 1;
 }
 
 /* Sum integers 1 to n */
-int sumtail (int n, int total)
-{
-    return 0;
+int sumtail(int n, int total) {
+    // basecase is at n==1.
+    
+    if (n == 1) {
+        return n + total;
+    } else {
+        return sumtail(n - 1, n + total);
+    }
 }
 
 /* Sum integers 1 to n */
-int sumwhile (int n)
-{
-  return 0;
+int sumwhile(int n) {
+    int total = 0;
+    // While that runs as long as n is not 0, n!=0
+    while (n != 0) {
+        total = total + n;
+        n--;
+    }
+    return total;
 }
-
