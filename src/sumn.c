908170443641c9c882e4727282f8987ac671f14c
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:47:47 +0100

    Update sumn.c

--- src/sumn.c
+++ src/sumn.c
@@ -1,8 +1,14 @@
 #include "sumn.h"
 
-#include<stdio.h>
+#include <stdio.h>
 
-int sumn (int n)
-{
+int sumn(int n) {
+    // basecase at n == 1.
+    if (n == 1) {
+        return n;
+    } else {
+       
+        return (2 * n - 1) + sumn(n - 1);
+    }
     return 0;
 }
