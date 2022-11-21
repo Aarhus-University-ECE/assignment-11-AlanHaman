5b5966824e60bf4eaffb82d121a5aa1adddff08c
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-21 16:38:17 +0100

    Update fib.c

--- src/fib.c
+++ src/fib.c
@@ -6,8 +6,27 @@
 #include <assert.h>		/* assert */
 #include <stdio.h>		/* printf */
 
+
+// fib 1 = 1
+// fib 2 = 1
+// fib 3 = 2
+// fib 4 = 3
+// fib 5 = 5
+
 /* Fibonacci function definition */
-int fib (int n, int p, int pp)
+int fib_tail (int n, int p,int pp)
 {
-    return 0;
+/* assert to make sure there is at least 1 fib number */
+assert (n >= 1);
+
+    if(n == 1)
+        return p;
+
+    else
+        return fib_tail(n-1,pp+p,p);
+}
+
+
+int fib(int n) {
+    return fib_tail(n, 1, 0);
 }
